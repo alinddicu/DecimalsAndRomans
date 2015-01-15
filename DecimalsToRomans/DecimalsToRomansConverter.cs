f92ff797903b0cfc0e13507db584de37781d1dd7
alinddicu <alinddicu@yahoo.com>
2015-01-15 22:31:46 +0100

    to base romans conversion ok

---
+++ DecimalsToRomans/DecimalsToRomansConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace DecimalsToRomans
+{
+    using System.Collections.Generic;
+
+    public class DecimalsToRomansConverter
+    {
+        public DecimalsToRomansConverter()
+        {
+            ConversionMap = new Dictionary<int, string>
+            {
+                { 1, "I" }, 
+                { 5, "V" }, 
+                { 10, "X" },
+                { 50, "L" },
+                { 100, "C" },
+                { 500, "L" },
+                { 1000, "M" }
+            };
+        }
+
+        public Dictionary<int, string> ConversionMap { get; private set; }
+
+        public string Convert(int dec)
+        {
+            return ConversionMap[dec];
+        }
+    }
+}
--- DecimalsToRomans/UnitTest1.cs
+++ DecimalsToRomans/UnitTest1.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace DecimalsToRomans
+﻿namespace DecimalsToRomans
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
     [TestClass]
     public class UnitTest1
     {
+        // I II III IV V VI VII VIII IX X
+        // X XX XXX XL L LI LII LIII IL C
+        // C CC CCC CD C DC DCC DCCC CM M
+        // M MM MMM
+
+        private readonly  DecimalsToRomansConverter _converter = new DecimalsToRomansConverter();
+
         [TestMethod]
-        public void TestMethod1()
+        public void GivenBaseDecimalsWhenConvertThenDecimalsConvertToRomans()
         {
+            var conversionMap = _converter.ConversionMap;
+
+            foreach(var dec in conversionMap.Keys)
+            {
+                Check.That(_converter.Convert(dec)).Equals(conversionMap[dec]);
+            }
         }
     }
 }
