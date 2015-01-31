48208ead73376e781ddd72796120a79384967f7b
alinddicu <alinddicu@yahoo.com>
2015-01-31 19:45:27 +0100

    something works from romans to decimals

---
+++ DecimalsAndRomans/RomansToDecimals/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace RomansToDecimals
+{
+    using System.Collections.Generic;
+
+    public class Converter
+    {
+        private static readonly string[] RomanBaseDigit = new[]
+        {
+            "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
+        };
+
+        private static readonly Dictionary<string, int> ConversionMap = new Dictionary<string, int>
+        {
+            {"I", 1}, {"II", 2}, {"III", 3}, {"IV", 4},{ "V", 5}, {"VI",6}, {"VII",7}, {"VIII",8}, {"IX", 9},
+            {"X", 10}
+            //{"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            //{"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            //{"M", "MM", "MMM", "", "", "", "", "", ""}
+        };
+
+        public int Convert(string romanNumeral)
+        {
+            if (romanNumeral == string.Empty)
+            {
+                return 0;
+            }
+
+            var groupAtTheEnd = ExtractGreatestRomanDigit(romanNumeral);
+            var theRest = romanNumeral.Substring(0, romanNumeral.Length - groupAtTheEnd.Length);
+
+            return ConversionMap[groupAtTheEnd] + Convert(theRest);
+        }
+
+        public string ExtractGreatestRomanDigit(string romanNumeral)
+        {
+            var endsWithMatchs = RomanBaseDigit.Where(romanNumeral.EndsWith).ToList();
+            var maxRomanCharsGroupCountInMatches = endsWithMatchs.Select(m1 => m1.ToCharArray().Count()).Max(charsCount => charsCount);
+            endsWithMatchs = endsWithMatchs.Where(m => m.ToCharArray().Length == maxRomanCharsGroupCountInMatches).ToList();
+
+            var maxDecimalValue = endsWithMatchs.Select(m => ConversionMap[m]).Max(o => o);
+
+            return ConversionMap.Keys.Single(k => ConversionMap[k] == maxDecimalValue);
+        }
+    }
+}
---
+++ DecimalsAndRomans/RomansToDecimals/ConverterTest.cs
@@ -0,0 +1,42 @@
+﻿using NFluent;
+
+namespace RomansToDecimals
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConverterTest
+    {
+        private readonly static Converter Converter = new Converter();
+
+        [TestMethod]
+        public void WhenIThenReturn1()
+        {
+            Check.That(Converter.Convert("I")).IsEqualTo(1);
+        }
+
+        [TestMethod]
+        public void WhenXIThenReturn11()
+        {
+            Check.That(Converter.Convert("XI")).IsEqualTo(11);
+        }
+
+        [TestMethod]
+        public void WhenXThenReturn10()
+        {
+            Check.That(Converter.Convert("X")).IsEqualTo(10);
+        }
+
+        [TestMethod]
+        public void WhenXIIIThenReturn13()
+        {
+            Check.That(Converter.Convert("XIII")).IsEqualTo(13);
+        }
+
+        [TestMethod]
+        public void WhenXIVThenReturn14()
+        {
+            Check.That(Converter.Convert("XIV")).IsEqualTo(14);
+        }
+    }
+}
