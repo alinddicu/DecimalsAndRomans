3a09da3791e13219d814d4b426b8a2c3e79e04e9
alinddicu <alinddicu@yahoo.com>
2015-01-31 18:26:09 +0100

    got it... somewhere

---
+++ DecimalsAndRomans/DecimalsToRomans/RomanBaseTenConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace DecimalsToRomans
+{
+    using System.Linq;
+
+    public class RomanBaseTenConverter
+    {
+        private readonly string[,] BaseTenRomans = new string[,]
+        {
+            {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            {"", "M", "MM", "MMM", "", "", "", "", "", ""}
+        };
+
+        public string Convert(int numeral)
+        {
+            var digitsFromNumeral = numeral
+                .ToString(CultureInfo.CurrentCulture)
+                .ToCharArray()
+                .Reverse()
+                .Select(c => c.ToString(CultureInfo.CurrentCulture))
+                .ToArray();
+            var digitsNumber = digitsFromNumeral.Length;
+
+            var romanNumeral = string.Empty;
+            while (digitsNumber > 0)
+            {
+                digitsNumber--;
+                var xTable = digitsNumber;
+                var yTable = int.Parse(digitsFromNumeral[digitsNumber]);
+                romanNumeral += BaseTenRomans[xTable, yTable];
+            }
+
+            return romanNumeral;
+        }
+    }
+}
---
+++ DecimalsAndRomans/DecimalsToRomans/RomanBaseTenConverterTest.cs
@@ -0,0 +1,28 @@
+﻿namespace DecimalsToRomans
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class RomanBaseTenConverterTest
+    {
+        // I II III IV V VI VII VIII IX X
+        // X XX XXX XL L LI LII LIII IL C
+        // C CC CCC CD C DC DCC DCCC CM M
+        // M MM MMM
+
+        private readonly  RomanBaseTenConverter _converter = new RomanBaseTenConverter();
+
+        [TestMethod]
+        public void WhenConvert2ThenReturnII()
+        {
+            Check.That(_converter.Convert(2)).Equals("II");
+        }
+        
+        [TestMethod]
+        public void WhenConvert3046ThenReturnMMMXLVI()
+        {
+            Check.That(_converter.Convert(3046)).Equals("MMMXLVI");
+        }
+    }
+}
